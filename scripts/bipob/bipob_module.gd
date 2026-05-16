8f5211ca63edf7fb40889df43729fffb6a300a9b
bageus <36338626+bageus@users.noreply.github.com>
2026-05-16 04:11:58 +0300

    Update bipob_module.gd

--- scripts/bipob/bipob_module.gd
+++ scripts/bipob/bipob_module.gd
@@ -1,9 +1,19 @@
 extends Resource
 class_name BipobModule
 
+<<<<<<< HEAD
 @export var id: String = ""
 @export var display_name: String = ""
 @export var description: String = ""
+=======
+var id: String = ""
+var display_name: String = ""
+var description: String = ""
+var energy_bonus: int = 0
+var actions_bonus: int = 0
+var vision_bonus: int = 0
+var granted_commands: Array[String] = []
+>>>>>>> 916233b84424b88795241bbaab5f1b126863d586
 
 @export var granted_commands: Array[String] = []
 
