33d4532dc7fe63999152c33aa84be26523b922fd
bageus <36338626+bageus@users.noreply.github.com>
2026-05-16 02:40:22 +0200

    BIB-103 add found-module install flow in box screen

--- scripts/bipob/bipob_module.gd
+++ scripts/bipob/bipob_module.gd
@@ -2,6 +2,11 @@ extends RefCounted
 class_name BipobModule
 
 var id: String = ""
+var display_name: String = ""
+var description: String = ""
+var energy_bonus: int = 0
+var actions_bonus: int = 0
+var vision_bonus: int = 0
 var granted_commands: Array[String] = []
 
 func _init(module_id: String = "", commands: Array[String] = []) -> void:
