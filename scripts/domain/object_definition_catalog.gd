7ed6d20df4b81f0ecc88e8fc26fb5eff9de14dd6
bageus <36338626+bageus@users.noreply.github.com>
2026-06-18 20:08:30 +0200

    newbip: add object definition catalog skeleton

---
+++ scripts/domain/object_definition_catalog.gd
@@ -0,0 +1,18 @@
+extends RefCounted
+
+# Target class: ObjectDefinitionCatalog
+# Загружает и выдаёт ObjectDefinition. UI не должен иметь свой hardcoded catalog.
+
+var definitions_by_id: Dictionary = {}
+
+func load_all(_base_path: String = "res://data/objects") -> void:
+	pass
+
+func register_definition(definition_id: String, definition: Dictionary) -> void:
+	definitions_by_id[definition_id] = definition
+
+func get_definition(definition_id: String) -> Dictionary:
+	return Dictionary(definitions_by_id.get(definition_id, {}))
+
+func get_all_definitions() -> Array:
+	return definitions_by_id.values()
