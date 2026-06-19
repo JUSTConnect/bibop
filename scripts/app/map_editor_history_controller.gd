b5e14da4ba51752e98edd1e05772e34cf265a489
bageus <36338626+bageus@users.noreply.github.com>
2026-06-19 08:16:21 +0200

    PR-14: use clear map command with editor state

--- scripts/app/map_editor_history_controller.gd
+++ scripts/app/map_editor_history_controller.gd
@@ -1,12 +1,9 @@
 extends "res://scripts/app/map_editor_controller.gd"
 
-const ReplaceWorldCommandRef = preload("res://scripts/map_constructor/commands/replace_world_command.gd")
+const ClearMapCommandRef = preload("res://scripts/map_constructor/commands/clear_map_command.gd")
 
 func clear_map_keep_palette() -> void:
 	if app_mode() != "edit":
 		return
-	var empty_world: Array[Dictionary] = []
-	var command: RefCounted = ReplaceWorldCommandRef.new().setup(repository, empty_world, "Clear map")
+	var command: RefCounted = ClearMapCommandRef.new().setup(repository, state)
 	history.call("execute", command)
-	state.call("clear_instance_selection")
-	state.set("next_instance_index", 1)
