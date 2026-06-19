a16e192cbecb75ac4f11a678fbcecabafef2f445
bageus <36338626+bageus@users.noreply.github.com>
2026-06-19 08:26:49 +0200

    PR-11: call test room controller dynamically

--- scripts/app/test_room_root.gd
+++ scripts/app/test_room_root.gd
@@ -8,5 +8,5 @@ func _ready() -> void:
 	set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
 	clip_contents = true
 	app_controller = AppControllerRef.new()
-	app_controller.setup(self)
+	app_controller.call("setup", self)
 	app_controller.call("_load_test_room")
