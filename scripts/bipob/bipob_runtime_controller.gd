a71d1e064c9ba17de5130249e9f86caef6bc6739
bageus <36338626+bageus@users.noreply.github.com>
2026-07-03 00:12:24 +0200

    Add Bipob runtime controller compatibility shim

---
+++ scripts/bipob/bipob_runtime_controller.gd
@@ -0,0 +1,6 @@
+extends BipobController
+class_name BipobRuntimeController
+
+const BipobModuleCatalogRuntimeRef = preload("res://scripts/bipob/bipob_module_catalog.gd")
+
+var EXTERNAL_SIDE_ORDER: Array[String] = BipobModuleCatalogRuntimeRef.EXTERNAL_SIDE_ORDER
